bd6302c41ddc14c4105318f847d68bb4848f7adc
AliReza <heidari.alirezad@gmail.com>
2024-09-04 10:55:18 +0330

    nothing

--- Organizer-panel/js/nav-responsive.js
+++
@@ -1,9 +0,0 @@
-/*               nav in responsive codes                */
-
-$('#bars-icon').on('click', function () {
-    $('#nav').css('right', '0px');
-});
-
-$('#nav-close-icon').on('click', function () {
-    $('#nav').css('right', '-300px');
-});
